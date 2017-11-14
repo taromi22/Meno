0d95c1fac7865bef5564816aadf4eb68418da52a
taromi22 <taromie@gmail.com>
2017-11-14 21:47:26 +0900

    Initial Commit

---
+++ Meno/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Meno
+//
+//  Created by 柳谷太郎 on 2017/11/14.
+//  Copyright © 2017年 Taro Yanagiya. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Meno/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Meno
+//
+//  Created by 柳谷太郎 on 2017/11/14.
+//  Copyright © 2017年 Taro Yanagiya. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
